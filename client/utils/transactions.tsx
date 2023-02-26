670636c28af2f1161be532f4977eaab8123472e7
vlawmz <flawmz@gmail.com>
2023-02-25 20:16:44 -0700

    Add the rest of the transactions

--- client/utils/transactions.tsx
+++ client/utils/transactions.tsx
@@ -1,6 +1,6 @@
 import '../flow/config';
 
-export async function createWall(fcl) {
+export async function createWall(fcl: any) {
   const txId = await fcl.mutate({
     cadence: `
 import FlowWall from 0xf3fcd2c1a78f5eee
@@ -27,14 +27,100 @@ transaction {
   console.log(tx);
 }
 
-export async function postWall() {
-  //
+export async function postWall(fcl: any, message: string) {
+  const txId = await fcl.mutate({
+    cadence: `
+import FlowWall from 0xf3fcd2c1a78f5eee
+
+transaction(address: Address, content: String) {
+
+  let authAccount: AuthAccount;
+  let wall: &AnyResource{FlowWall.WallPublic};
+
+  prepare(acct: AuthAccount) {
+    self.authAccount = acct;
+    let wallAccount = getAccount(address);
+    let wall_ref = wallAccount.getCapability<&{FlowWall.WallPublic}>(/public/Wall)
+    let wall = wall_ref.borrow()!
+    self.wall = wall;
+  }
+
+  execute {
+    log(self.wall.sendMessage(sender: self.authAccount, content: content))
+  }
+}`,
+    args: (arg, t) => [arg(message, t.String)],
+    payer: fcl.authz,
+    proposer: fcl.authz,
+    authorizations: [fcl.authz],
+    limit: 50,
+  });
+
+  const tx = await fcl.tx(txId).onceSealed();
+  console.log(tx);
 }
 
-export async function deleteMessage() {
-  //
+export async function deleteMessage(fcl: any, timestamp: string) {
+  const txId = await fcl.mutate({
+    cadence: `
+import FlowWall from 0xf3fcd2c1a78f5eee
+
+transaction(timestamp: UFix64) {
+  
+  let authAccount: AuthAccount
+  
+  prepare(authAccount: AuthAccount) {
+    self.authAccount = authAccount
+  }
+
+  execute {
+    let wall <- self.authAccount.load<@FlowWall.Wall>(from: /storage/Wall)!
+    wall.deleteMessage(timestamp: timestamp)
+    self.authAccount.save(<- wall, to: /storage/Wall)
+  }
+}`,
+    args: (arg, t) => [arg(timestamp, t.UFix64)],
+    payer: fcl.authz,
+    proposer: fcl.authz,
+    authorizations: [fcl.authz],
+    limit: 50,
+  });
+
+  const tx = await fcl.tx(txId).onceSealed();
+  console.log(tx);
 }
 
-export async function banUser() {
-  //
+export async function banUser(fcl: any, address: string) {
+  const txId = await fcl.mutate({
+    cadence: `
+import FlowWall from 0xf3fcd2c1a78f5eee
+
+transaction(address: Address) {
+  
+  let authAccount: AuthAccount
+  
+  prepare(authAccount: AuthAccount) {
+    self.authAccount = authAccount
+  }
+
+  pre {
+    self.authAccount.address != address: "You cannot ban yourself"
+  }
+
+  execute {
+    let wall <- self.authAccount.load<@FlowWall.Wall>(from: /storage/Wall)!
+    wall.banAddress(ban: address)
+    self.authAccount.save(<- wall, to: /storage/Wall)
+  }
+}
+`,
+    args: (arg, t) => [arg(address, t.Address)],
+    payer: fcl.authz,
+    proposer: fcl.authz,
+    authorizations: [fcl.authz],
+    limit: 50,
+  });
+
+  const tx = await fcl.tx(txId).onceSealed();
+  console.log(tx);
 }
