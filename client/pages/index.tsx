886ffd7757bec8db85d9c6f09570f79206b86e13
vlawmz <flawmz@gmail.com>
2023-02-25 21:22:17 -0700

    Merge main

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,14 +3,22 @@ import styles from '../styles/Home.module.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import MainView from '@/components/MainView';
-import { useState, useEffect } from 'react';
-import * as fcl from "@onflow/fcl";
-import "../flow/config";
+import { useState, useEffect, useCallback } from 'react';
+import * as fcl from '@onflow/fcl';
+import '../flow/config';
+import { createWall, wallExists } from '../utils/transactions.tsx';
 
 export default function Home() {
-  const [user, setUser] = useState({loggedIn: null, addr: null})
+  const [user, setUser] = useState({ loggedIn: null, addr: null });
 
-  useEffect(() => fcl.currentUser.subscribe(setUser), [])
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+
+  const [userWall, setUserWall] = useState<any>(null);
+
+  useEffect(() => fcl.currentUser.subscribe(setUser), []);
+  useEffect(() => {
+    setUserWall(user.addr);
+  }, [user]);
 
   return (
     <div className="layout">
@@ -18,12 +26,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Navbar user={user} />
+        <Navbar user={user} setUserWall={setUserWall} />
         <div className={styles.container}>
-          <MainView />
+          <MainView user={user} userWall={userWall} />
         </div>
       </div>
-      <Footer />
+      <Footer user={user} fcl={fcl} />
     </div>
   );
 }
