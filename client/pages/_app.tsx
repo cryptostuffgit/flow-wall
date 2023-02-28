cd0759e6e9130a1cb41a6feb26e22d7f1ea3a520
vlawmz <flawmz@gmail.com>
2023-02-28 13:54:16 -0700

    Fix global css import

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import '../styles/Main.scss'
+import '../styles/globals.css';
+import '../styles/Main.scss';
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
