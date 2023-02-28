86401b7a86fac5041e5ef3b5a51c9ef6d0846cfa
vlawmz <flawmz@gmail.com>
2023-02-28 11:02:46 -0700

    Fix shit

--- client/components/MainView/index.tsx
+++ client/components/MainView/index.tsx
@@ -3,7 +3,6 @@ import { useWallExists } from '../../utils/transactions';
 import { ToastContainer } from 'react-nextjs-toast';
 import * as fcl from '@onflow/fcl';
 import Wall from '@/components/Wall';
-import WallAdmin from '@/components/WallAdmin';
 import CreateWall from '@/components/CreateWall';
 
 const MainView = ({ user, userAddress }) => {
@@ -17,37 +16,17 @@ const MainView = ({ user, userAddress }) => {
 
   const isYou = user.addr === userAddress;
   const isAdmin = isYou && wallExists[0];
-  const needsMigrate = wallExists[0] && !wallExists[1];
 
   return (
     <div className="text-container main-container">
       <ToastContainer />
-      <h1 className={'heading' + (isAdmin ? ' admin' : '')}>
-        <p>
-          {isYou && wallExists[0] ? (
-            <>Your Wall</>
-          ) : userAddress && wallExists[0] ? (
-            <>{userAddress}'s Wall</>
-          ) : userAddress && !wallExists[0] ? (
-            <>{userAddress} has no wall!</>
-          ) : (
-            <>Search for an Address</>
-          )}
-        </p>
-        {user.addr && isYou && !wallExists[0] ? (
-          <CreateWall user={user} address={userAddress} isYou={isYou} />
-        ) : user.addr && !isYou && !wallExists[0] ? (
-          <CreateWall user={user} address={userAddress} isYou={isYou} />
-        ) : (
-          <></>
-        )}
-        {wallExists[0] && isAdmin && (
-          <WallAdmin needsMigrate={!wallExists[1]} />
-        )}
-      </h1>
-      {wallExists[0] && (
-        <Wall user={user} address={userAddress} admin={isAdmin} />
-      )}
+      <Wall
+        wallExists={wallExists}
+        isYou={isYou}
+        user={user}
+        address={userAddress}
+        admin={isAdmin}
+      />
     </div>
   );
 };
--- client/components/Wall/index.tsx
+++ client/components/Wall/index.tsx
@@ -3,6 +3,8 @@ import * as fcl from '@onflow/fcl';
 import { postWall, getWall } from '../../utils/transactions';
 import TextInput from '../TextInput';
 import MessageView from '../Message';
+import WallAdmin from '@/components/WallAdmin';
+import CreateWall from '@/components/CreateWall';
 
 type Message = {
   sender: String;
@@ -18,19 +20,21 @@ type Wall = {
   banned: String[];
 };
 
-const Wall = ({ user, address, admin }) => {
+const Wall = ({ wallExists, needsMigrate, isYou, user, address, admin }) => {
   const [wall, setWall] = useState<Wall | any>({});
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
-    (async () => {
-      const fcl_wall = await getWall(fcl, address);
-      setWall(fcl_wall);
-      let fcl_messages = fcl_wall.messages;
-      fcl_messages = fcl_messages.sort((a, b) => b.timestamp - a.timestamp);
-      setMessages(fcl_messages);
-    })();
-  }, [address]);
+    if (wallExists[0]) {
+      (async () => {
+        const fcl_wall = await getWall(fcl, address);
+        setWall(fcl_wall);
+        let fcl_messages = fcl_wall.messages;
+        fcl_messages = fcl_messages.sort((a, b) => b.timestamp - a.timestamp);
+        setMessages(fcl_messages);
+      })();
+    }
+  }, [address, wallExists]);
 
   const postMessage = (mesageText) => {
     postWall(fcl, mesageText, address).then(() => {
@@ -44,6 +48,27 @@ const Wall = ({ user, address, admin }) => {
 
   return (
     <>
+      <h1 className={'heading' + (admin ? ' admin' : '')}>
+        <p>
+          {isYou && wallExists[0] ? (
+            <>Your Wall</>
+          ) : address && wallExists[0] ? (
+            <>{address}'s Wall</>
+          ) : address && !wallExists[0] ? (
+            <>{address} has no wall!</>
+          ) : (
+            <>Search for an Address</>
+          )}
+        </p>
+        {user.addr && isYou && !wallExists[0] ? (
+          <CreateWall user={user} address={address} isYou={isYou} />
+        ) : user.addr && !isYou && !wallExists[0] ? (
+          <CreateWall user={user} address={address} isYou={isYou} />
+        ) : (
+          <></>
+        )}
+        {wallExists[0] && admin && <WallAdmin needsMigrate={!wallExists[1]} />}
+      </h1>
       <div className="wall">
         <div className="wall_props">
           <div className="avatar">
