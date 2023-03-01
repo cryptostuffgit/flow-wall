6a6bf5764328bb7f125174607dfb5878aef73f7e
cryptostuffgit <ethan.itovitch@frontier3.io>
2023-02-28 18:06:15 -0600

    yo

--- client/hooks/contract.ts
+++ client/hooks/contract.ts
@@ -1,31 +1,31 @@
-// import { useState, useEffect } from 'react';
-// import * as fcl from '@onflow/fcl';
-// import { getWall } from '../utils/transactions';
+import { useState, useEffect } from 'react';
+import * as fcl from '@onflow/fcl';
+import { getWall } from '../utils/transactions';
 
-// const useProfileWall = async (address: string) => {
-//   const [profileWall, setProfileWall] = useState(null);
-//   const fcl_wall = await getWall(fcl, address);
+export const useProfileWall = async (address: string) => {
+  // const [profileWall, setProfileWall] = useState(null);
+  // const fcl_wall = await getWall(fcl, address);
 
-//   setProfileWall(fcl_wall)
+  // setProfileWall(fcl_wall)
 
-//   return {
-//     profileWall,
-//     addMessage,
-//     addCanvas,
-//     setProfileWall
-//   }
-// }
+  // return {
+  //   profileWall,
+  //   addMessage,
+  //   addCanvas,
+  //   setProfileWall
+  // }
+}
 
-// const useCreatedWalls = async () => {
-//   const [profileWall, setProfileWall] = useState(null);
-//   const fcl_wall = await getWall(fcl, address);
+export const useCreatedWalls = async () => {
+  // const [profileWall, setProfileWall] = useState(null);
+  // const fcl_wall = await getWall(fcl, address);
 
-//   setProfileWall(fcl_wall)
+  // setProfileWall(fcl_wall)
 
-//   return {
-//     profileWall,
-//     addMessage,
-//     addCanvas,
-//     setProfileWall
-//   }
-// }
+  // return {
+  //   profileWall,
+  //   addMessage,
+  //   addCanvas,
+  //   setProfileWall
+  // }
+}
