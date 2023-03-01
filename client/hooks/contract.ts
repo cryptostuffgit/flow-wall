1e30926b97d179462b5e395bea86ebfe8c47e811
cryptostuffgit <ethan.itovitch@frontier3.io>
2023-02-28 18:05:25 -0600

    yo

---
+++ client/hooks/contract.ts
@@ -0,0 +1,31 @@
+// import { useState, useEffect } from 'react';
+// import * as fcl from '@onflow/fcl';
+// import { getWall } from '../utils/transactions';
+
+// const useProfileWall = async (address: string) => {
+//   const [profileWall, setProfileWall] = useState(null);
+//   const fcl_wall = await getWall(fcl, address);
+
+//   setProfileWall(fcl_wall)
+
+//   return {
+//     profileWall,
+//     addMessage,
+//     addCanvas,
+//     setProfileWall
+//   }
+// }
+
+// const useCreatedWalls = async () => {
+//   const [profileWall, setProfileWall] = useState(null);
+//   const fcl_wall = await getWall(fcl, address);
+
+//   setProfileWall(fcl_wall)
+
+//   return {
+//     profileWall,
+//     addMessage,
+//     addCanvas,
+//     setProfileWall
+//   }
+// }
