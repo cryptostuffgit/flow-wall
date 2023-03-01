eeac363d97fce1d40bc21601c72c9b1208360a45
cryptostuffgit <ethan.itovitch@frontier3.io>
2023-02-28 18:32:37 -0600

    yo

--- client/flow/config.js
+++ client/flow/config.js
@@ -1,7 +1,7 @@
 import { config } from '@onflow/fcl';
 
 config({
-  "accessNode.api": "access.devnet.nodes.onflow.org:9000", // Mainnet: "https://rest-mainnet.onflow.org/"
+  "accessNode.api": "https://rest-devnet.onflow.org/", // Mainnet: "https://rest-mainnet.onflow.org/"
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
   "0xFlowWall": "0xf492380dc41f13fde"
 })
